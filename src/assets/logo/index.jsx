23534476c9e1c9e166c51dee71985bd3cc02409d
jag cheema <jagmitsingh2003@gmail.com>
2023-03-20 19:57:53 -0400

    added images, and resume pdf

---
+++ src/assets/logo/index.jsx
@@ -0,0 +1,21 @@
+import HtmlLogo from "./html.png";
+import CssLogo from "./css.png";
+import JavascriptLogo from "./javascript.png";
+import NodejsLogo from "./nodejs.png";
+import PostgreLogo from "./postgre.png";
+import GraphqlLogo from "./graphql.png";
+import ReactLogo from "./react.png";
+import ReduxLogo from "./redux.png";
+
+const Logos = {
+  HtmlLogo,
+  CssLogo,
+  JavascriptLogo,
+  NodejsLogo,
+  PostgreLogo,
+  GraphqlLogo,
+  ReactLogo,
+  ReduxLogo,
+};
+
+export default Logos;
