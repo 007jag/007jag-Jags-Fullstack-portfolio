7597750caa8e5067a43682c2993fec187ad19aa5
jag cheema <jagmitsingh2003@gmail.com>
2023-03-20 21:24:40 -0400

    finished footer section

---
+++ src/components/footer.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import GithubIcon from "@mui/icons-material/GitHub";
+import LinkedinIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+function Footer() {
+  return (
+    <div className="flex flex-col items-center p-5 bg-secondary">
+      <div className="text-[1.2rem] font-semibold">
+        Copyright © 2023 Jagmit Cheema
+      </div>
+      <div className="mt-5 flex items-center space-x-5">
+        <GithubIcon
+          className="hover:scale-[1.1] transition-all cursor-pointer text-[2.5rem]"
+          onClick={() => window.open("github.com")}
+        />
+        <LinkedinIcon
+          className="hover:scale-[1.1] transition-all cursor-pointer text-[2.5rem]"
+          onClick={() => window.open("github.com")}
+        />
+        <TwitterIcon
+          className="hover:scale-[1.1] transition-all cursor-pointer text-[2.5rem]"
+          onClick={() => window.open("github.com")}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,7 @@
-import { useEffect, useRef, useState } from "react";
-import JcLogo from "../assets/jc_logo.png";
-import { Button } from "@mui/material";
-
-import InstagramIcon from "@mui/icons-material/Instagram";
-import LinkedinIcon from "@mui/icons-material/LinkedIn";
-import GithubIcon from "@mui/icons-material/GitHub";
-
-import PersonIcon from "@mui/icons-material/Person";
-import ProjectIcon from "@mui/icons-material/Receipt";
-import ContactIcon from "@mui/icons-material/Phone";
-import PdfIcon from "@mui/icons-material/PictureAsPdf";
-
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import React from "react";
 
 function Header() {
-  
+  return <div>Header</div>;
 }
 
 export default Header;
