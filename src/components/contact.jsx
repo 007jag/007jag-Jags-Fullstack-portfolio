51d5f56f2c4c603f8fd443a38da9fcb2116aba47
jag cheema <jagmitsingh2003@gmail.com>
2023-03-20 21:12:23 -0400

    added contact section in contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+    return (
+        <div
+          id="contact"
+          className="bg-background text-white p-5 py-16 md:text-[2rem] font-semibold"
+        >
+          <div className="flex flex-col items-center">
+            <div className="md:text-[2.5rem] font-bold">Contact Me</div>
+            <div>Email : Jagmitsingh2003@gmail.com</div>
+            <div>Phone : 704-303-6290</div>
+          </div>
+        </div>
+      );
 }
 
 export default Contact;
